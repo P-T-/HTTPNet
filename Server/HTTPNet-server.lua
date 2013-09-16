90e3b92f1f6fd466882aa89253bd70fbc6e4b1ea
PixelToast <andre.lipke@gmail.com>
2013-09-16 15:03:03 -0400

    Update

--- Server/HTTPNet-server.lua
+++ Server/HTTPNet-server.lua
@@ -9,8 +9,8 @@
 --    comments
 --    moar config options
 --    error checking
---    timeouts
 --    clean up a bit
+--    add a reconnect buffer so messages arent lost in the short time the client is offline
 ------------------------------------------------
 
 ------------------------------------------------
@@ -39,6 +39,13 @@ local function getmax(tbl)
 		end
 	end
 	return mx
+end
+local function genuid()
+	local o=""
+	for l1=1,16 do
+		o=o..string.format("%X",math.random(1,15))
+	end
+	return o
 end
 local function serialize(...)
 	local t={...}
@@ -107,9 +114,39 @@ local function close(cl)
 	cli[cl.i]=nil
 end
 local function serve(cl,dat)
-	cl.s:send("HTTP/1.1 337 Potato\r\nContent-Length: "..dat:len().."\r\n\r\n"..dat)
+	cl.s:send("HTTP/1.1 337 H4X\r\nContent-Length: "..dat:len().."\r\n\r\n"..dat) -- nothing to see here
 	close(cl)
 end
+local function req(cl,s)
+	local t=unserialize(s)
+	if cl.postlen and not t then
+		close(cl)
+	else
+		if cl.uri=="ping" then
+			serve(cl,serialize("pong",genuid()))
+		elseif cl.uri=="send" and t[3] then
+			print("'"..serialize(t[1]).."' is sending '"..serialize(t[3]).."' to '"..serialize(t[2]).."'")
+			for k,v in pairs(queue) do
+				if v.id==t[2] then
+					serve(v,serialize(v.uid,"msg",t[1],t[3]))
+					queue[k]=nil
+				end
+			end
+			serve(cl,"")
+		elseif cl.uri=="receive" and t[1] then
+			if not cl.uid then
+				print("Bad request "..cl.id)
+			else
+				print("'"..serialize(t[1]).."' is receiving")
+				cl.id=t[1]
+				table.insert(queue,cl)
+			end
+		else
+			close(cl)
+		end
+	end
+end
+local ltime=os.clock()
 while true do
 	local s=sv:accept()
 	while s do
@@ -117,50 +154,48 @@ while true do
 		local a=1
 		while true do
 			if not cli[a] then
-				cli[a]={s=s,head={},i=a}
+				cli[a]={s=s,head={},i=a,dt=0}
 				break
 			end
 			a=a+1
 		end
 		s=sv:accept()
 	end
+	local cdt=os.clock()-ltime
 	for i=1,getmax(cli) do
 		local cl=cli[i]
 		if cl then
+			cl.dt=cl.dt+cdt
+			if cl.dt>30 then
+				for k,v in pairs(queue) do
+					if v then
+						if v.s==cl then
+							queue[k]=nil
+						end
+					end
+				end
+				print("'"..cl.id.."' timed out")
+				serve(cl,serialize(cl.uid,"timeout"))
+			end
 			local s,e=cl.s:receive(0)
 			if e=="closed" and not s then
-				cl.s:close()
-				cli[i]=nil
+				close(cl)
 			else
 				local s,e=cl.s:receive(cl.postlen)
 				while s do
 					if cl.postlen then
-						local t=unserialize(s)
-						if not t then
-							close(cl)
-						else
-							if cl.uri=="send" and t[3] then
-								print("'"..serialize(t[1]).."' is sending '"..serialize(t[3]).."' to '"..serialize(t[2]).."'")
-								for k,v in pairs(queue) do
-									if v.id==t[2] then
-										serve(v,serialize(t[1],t[3]))
-										queue[k]=nil
-									end
-								end
-								serve(cl,"")
-							elseif cl.uri=="receive" and t[1] then
-								print("'"..serialize(t[1]).."' is receiving")
-								cl.id=t[1]
-								table.insert(queue,cl)
-							else
-								close(cl)
-							end
-						end
+						req(cl,s)
 					elseif s=="" then
 						local pg,d=cl.head[1]:match("^(.-) (.-) HTTP/1.1$")
-						if pg=="POST" and not cl.postlen then
-							cl.uri=d:sub(2)
-							cl.postlen=tonumber(prochead(cl)["Content-Length:"])
+						if not cl.postlen then
+							cl.uri=d:sub(2):gsub("(%?(.+))$","")
+							cl.uid=d:sub(2):match("%?(.+)$")
+							cl.rid=d
+							if pg=="POST" then
+								cl.postlen=tonumber(prochead(cl)["Content-Length:"])
+							else
+								req(cl,s)
+							end
 						else
 							close(cl)
 						end
@@ -176,9 +211,10 @@ while true do
 			end
 		end
 	end
+	ltime=os.clock()
 	local clt={sv}
 	for k,v in pairs(cli) do
 		table.insert(clt,v.s)
 	end
-	socket.select(clt,nil)
+	socket.select(clt,nil,10)
 end
